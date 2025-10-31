c7bfa0b8eb001fba951cd42388315a922bf83b17
Franchuphone <222453306+Franchuphone@users.noreply.github.com>
2025-10-31 18:38:16 +0100

    Improve comments

--- javascript.js
+++ javascript.js
@@ -1,51 +1,37 @@
-// Function to get the computer choice
-
 function getComputerChoice() {
 
-    // Choose a number between 0 and 1
-
+    // Using the random math variable to make a pseudo random election
+    
     let number = Math.random();
-
-    // Make a triple choice around this number
-    // IF less than 0.33 return Rock
 
     if (number <= 0.33) {
         return "ROCK"
     }
 
-    // IF less than 0.66 return Paper
-
     else if ((number >= 0.33) && (number <=0.66)) {
         return "PAPER";
     }
-
-    // IF more than 0.66 return Scissors
 
     else {
         return "SCISSORS";
     }
 }
 
-// Function to get the human choice
-
 function getHumanChoice () {
-
-    // Prompt user for choice
 
     let choice = prompt("Rock or Paper or Scissors?","");
 
-   // If user cancel prompt, stop function
+   // Stops function if click on prompt's cancel button
 
     if (choice == null) {
         return
     }
-
-    //Else capitalize all the word
     
     else {
-        choice = choice.toUpperCase();
 
-        // If choice is Rock or Paper or Scissors return choice
+        // Allows every lower or upper case variants of the rock/paper/scissors
+
+        choice = choice.toUpperCase();
 
         if (choice === "ROCK" || 
             choice === "PAPER" || 
@@ -53,7 +39,7 @@ function getHumanChoice () {
                 return choice;
             }
 
-        // ELse prompt again
+        // Forces the use of the correct words rock/paper/scissors
 
         else {
             alert ("Not a correct value, please use only Rock or Paper or Scissors");
@@ -62,44 +48,25 @@ function getHumanChoice () {
     }
 }
 
-// Function to play a single round
-
-function playRound ( humanChoice , computerChoice , roundNumber) {
-
-    // Compare the human choice to computer choice
-    // IF human same as computer 
+function playOneRound ( humanChoice , computerChoice , roundNumber) {
 
     if ( humanChoice === computerChoice ) {
-
-        //     Return Draw
-        alert (`DRAW !!
+            alert (`DRAW !!
             Your score : ${humanScore}
             Computer score : ${computerScore}`)
     }
-
-    // Else if human is Rock and computer is Scissors
-    //     Or human is Paper and computer is Rock
-    //     Or human is Scissors and computer is Paper
 
     else if (((humanChoice === "ROCK") && (computerChoice === "SCISSORS")) ||
             ((humanChoice === "PAPER") && (computerChoice === "ROCK")) ||
             ((humanChoice === "SCISSORS") && (computerChoice === "PAPER"))) {
 
-            //     Increment human score
-
             humanScore += 1;
-
-            //     Return Win
-
             alert (`Round : ${roundNumber}
                 YOU WIN !!
                 Your score : ${humanScore}
                 Computer score : ${computerScore}`);
 
         }
-
-    // Else return Lose
-    // Increment computer score
 
     else {
         computerScore += 1;
@@ -110,49 +77,38 @@ function playRound ( humanChoice , computerChoice , roundNumber) {
         }
 }
 
-// Declare variables for players score
+// Initializes all needed variables
 
 let humanScore = 0;
 let computerScore = 0;
-
-//Declare variables for players choices
-
 let humanChoice = "";
 let computerChoice = "";
+let roundsNumber = +prompt("How many rounds do you want to play?", "") 
 
-// Declare variable asking player for the number of rounds
+// Let's play the game !!
 
-let rounds = +prompt("How many rounds do you want to play?", "") 
-
-// Play 1 round x times defined by user
-
-for (let i=0 ; i < rounds ; i++) {
-
-    // Allocate choices functions returns to players choice variables
+for (let i=0 ; i < roundsNumber ; i++) {
 
     humanChoice = getHumanChoice();
     computerChoice = getComputerChoice();
 
-    // If user cancel election prompt, stop loop
+    // Stops iterations if click on prompt's cancel button
 
     if (humanChoice === undefined) {
         break;
     }
 
-    //Else keep playing
-
     else {
-        playRound (humanChoice,computerChoice,i+1)
+        playOneRound (humanChoice,computerChoice,i+1)
     }
 }
 
-// If users cancel any prompt, say goodbye
+// Displays goodbye message if any prompt's cancel button is clicked
+// Displays goodbye message also when user don't choose any rounds or choose 0 round
 
-if (rounds == 0 || humanChoice === undefined) {
+if (roundsNumber == 0 || humanChoice === undefined) {
         alert ("See you soon Space Cowboy !!")
     }
-    
-// Else display the final score
 
 else {
     alert (`FINAL SCORE 
