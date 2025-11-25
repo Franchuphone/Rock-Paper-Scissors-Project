7f16d587ffce78df0bfa36a17d589e082c7c5938
Franchuphone <222453306+Franchuphone@users.noreply.github.com>
2025-11-25 23:06:53 +0100

    Add new UI

--- javascript.js
+++ javascript.js
@@ -1,111 +1,112 @@
 function getComputerChoice() {
-    
+
     const number = Math.random();
 
-    if (number <= (1/3)) {
+    if ( number <= ( 1 / 3 ) ) {
         return "ROCK"
     }
-
-    else if (number >= (2/3)) {
+    else if ( number >= ( 2 / 3 ) ) {
         return "PAPER";
     }
-
     else {
         return "SCISSORS";
     }
 }
 
-function getHumanChoice () {
-
-    let choice = prompt("Rock or Paper or Scissors?","");
-
-   // Stops function if click on prompt's cancel button
-
-    if (!choice) {
-        return
-    }
-    
-    else {
-
-        // Allows every lower or upper case variants of the rock/paper/scissors
-
-        choice = choice.toUpperCase();
-
-        if (choice === "ROCK" || 
-            choice === "PAPER" || 
-            choice === "SCISSORS") {
-                return choice;
-            }
-
-        // Forces the use of the correct words rock/paper/scissors
-
-        else {
-            alert ("Not a correct value, please use only Rock or Paper or Scissors");
-            return(getHumanChoice())
-        }
-    }
-}
-
-function playOneRound (roundNumber) {
+function playOneRound( humanChoice, computerChoice ) {
 
     if ( humanChoice === computerChoice ) {
-            alert (`Round : ${roundNumber}
-        DRAW !!
-        Your score : ${humanScore}
-        Computer score : ${computerScore}`)
+        return `DRAW !!`;
     }
+    else if ( ( ( humanChoice === "ROCK" ) && ( computerChoice === "SCISSORS" ) ) ||
+        ( ( humanChoice === "PAPER" ) && ( computerChoice === "ROCK" ) ) ||
+        ( ( humanChoice === "SCISSORS" ) && ( computerChoice === "PAPER" ) ) ) {
 
-    else if (((humanChoice === "ROCK") && (computerChoice === "SCISSORS")) ||
-            ((humanChoice === "PAPER") && (computerChoice === "ROCK")) ||
-            ((humanChoice === "SCISSORS") && (computerChoice === "PAPER"))) {
-
-            humanScore += 1;
-            alert (`Round : ${roundNumber}
-        YOU WIN !!
-        Your score : ${humanScore}
-        Computer score : ${computerScore}`);
-
-        }
-
+        humanScore++;
+        return `YOU WIN!!`;
+    }
     else {
-        computerScore += 1;
-        alert (`Round : ${roundNumber}
-        YOU LOSE !!
-        Your score : ${humanScore}
-        Computer score : ${computerScore}`);
-        }
+        computerScore++;
+        return `YOU LOSE !!`;
+    }
 }
 
-// Initializes all needed variables
+function displayResults( humanChoice, roundNumber ) {
+
+    const computerChoice = getComputerChoice();
+    const resultText = playOneRound( humanChoice, computerChoice )
+
+    round.textContent = `Round : ${ roundNumber }`;
+    result.textContent = resultText;
+    document.body.className = `${ humanChoice } `;
+    humanResult.textContent = `Your score: ${ humanScore }`;
+    computerResult.textContent = `Computer score: ${ computerScore } `;
+
+}
+
+function handleChoice( e ) {
+
+    displayResults( e.target.textContent, roundNumber );
+    displayLastRound();
+    roundNumber++;
+}
+
+function displayLastRound() {
+
+    if ( roundNumber >= roundTotal ) {
+        const finalRound = document.createElement( "div" );
+        finalRound.className = "round";
+        finalRound.style.marginTop = "2rem"
+        finalRound.textContent = "Final score !"
+        round.parentNode.insertBefore( finalRound, humanResult );
+        createNewGameBtn();
+        hideBtn();
+        // A complement of hiding buttons
+        buttons.forEach( ( button ) => {
+            button.removeEventListener( "click", handleChoice );
+        } );
+    }
+}
+
+function createNewGameBtn() {
+
+    const newGameBtn = document.createElement( "button" );
+    newGameBtn.textContent = "New game";
+    round.parentNode.appendChild( newGameBtn )
+    newGameBtn.addEventListener( "click", () => location.reload() )
+}
+
+function hideBtn() {
+
+    buttons.forEach( ( button ) => {
+        button.style.display = "none";
+    } );
+}
+
+function choiceBtn() {
+
+    buttons.forEach( ( button ) => {
+        button.addEventListener( "click", handleChoice );
+    } );
+}
 
 let humanScore = 0;
 let computerScore = 0;
-let humanChoice = "";
-let computerChoice = "";
-let roundsNumber = +prompt("How many rounds do you want to play?", "") 
+let roundNumber = 1;
+const roundTotal = +prompt( "How many rounds do you want to play?", "" );
+const buttons = document.querySelectorAll( "button" );
+const round = document.querySelector( ".round" );
+const result = document.querySelector( "#result" );
+const humanResult = document.querySelector( "#human-result" );
+const computerResult = document.querySelector( "#computer-result" );
 
-// Let's play the game !!
 
-for (let i=0 ; i < roundsNumber ; i++) {
-
-    humanChoice = getHumanChoice();
-    computerChoice = getComputerChoice();
-
-    // Stops iterations if click on prompt's cancel button
-
-    if (humanChoice === undefined) {
-        break;
-    }
-
-    else {
-        playOneRound (i+1)
-    }
+if ( !roundTotal ) {
+    alert( "See you soon Space Cowboy !!" );
+    createNewGameBtn();
+    hideBtn();
+} else {
+    round.textContent = "Play by clicking on a button above"
+    choiceBtn();
 }
 
-// Displays goodbye message if any prompt's cancel button is clicked
-// Displays goodbye message also when user don't choose any rounds or choose 0 round
-
-(!roundsNumber || !humanChoice) ? alert ("See you soon Space Cowboy !!") : 
-    alert (`FINAL SCORE 
-        Your score : ${humanScore}
-        Computer score : ${computerScore}`) ;
